31423cf3c9defa0bce77df170b3ce1b0a2f9acec
Mike <mskowron4@yahoo.com>
2022-12-05 22:26:35 -0800

    final tweaks

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -2,49 +2,51 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-
 router.get('/', async (req, res) => {
-try {
-  const categories = await Category.findAll({
-    include: [{ model: Product }],
-  });
-  res.status(200).json(categories);
-} catch (err) {
-  res.status(500).json(err);
-  console.log(err);
-}
+
+  try {
+    const categories = await Category.findAll({
+      
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', async (req, res) => {
+ 
   try {
-    const category = await Category.findByPk(req.params.id, {
+    const singleCategory = await Category.findByPk(req.params.id, {
       include: [{ model: Product }],
     });
 
-    if (!category) {
-      res.status(404).json({ message: 'Invalid id!' });
+    if (!singleCategory) {
+      res.status(404).json({ message: 'No category found with that id!' });
       return;
     }
-    res.status(200).json(category);
+
+    res.status(200).json(singleCategory);
   } catch (err) {
     res.status(500).json(err);
-    console.log(err);
   }
 });
 
 router.post('/', async (req, res) => {
+ 
   try {
-    const category = await Category.create({
+    const newCategory = await Category.create({
       category_name: req.body.category_name,
     });
-    res.status(200).json(category);
+    res.status(200).json(newCategory);
   } catch (err) {
     res.status(400).json(err);
-    console.log(err);
   }
 });
 
 router.put('/:id', async (req, res) => {
+  
   try {
     const updatedCategory = await Category.update(
       { category_name: req.body.category_name, },
@@ -57,11 +59,12 @@ router.put('/:id', async (req, res) => {
     res.status(200).json(updatedCategory);
   } catch (err) {
     res.status(400).json(err);
-    console.log(err);
   }
 });
 
+
 router.delete('/:id', async (req, res) => {
+  
   try {
     const deletedCategory = await Category.destroy({
       where: {
@@ -71,16 +74,6 @@ router.delete('/:id', async (req, res) => {
     res.status(200).json(deletedCategory);
   } catch (err) {
     res.status(400).json(err);
-    try {
-      const deleteCategory = await Category.destroy({
-        where: {
-          id: req.params.id,
-        }
-      });
-      res.status(200).json(deleteCategory);
-    } catch (err) {
-      res.status(400).json(err);
-    }
   }
 });
 
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # C13-E-commerce-Back-End
+Models and routes for e commerce site.
+# Instructions 
+npm i 
+npm run seed
+npm start
+
+# Video Link 
+https://drive.google.com/file/d/1FFbBDvkMTcQm5RBYUiam6uWwZXz-zFgC/view
