7158181f9e8a8d70c83cf6ada1e90cbaf33b83ab
Mike <mskowron4@yahoo.com>
2022-12-05 21:26:53 -0800

    added routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,34 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.findAll({
+      include: [{ model: Category }],
+    });
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {
+    const singleProduct = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }],
+    });
+
+    if (!singleProduct) {
+      res.status(404).json({ message: 'Invalid id!' });
+      return;
+    }
+    res.status(200).json(singleProduct);
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err);
+  }
 });
 
 // create new product
@@ -84,13 +103,24 @@ router.put('/:id', (req, res) => {
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
       res.status(400).json(err);
+      console.log(err);
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const deletedProduct = await Product.destroy({
+      where: {
+        id: req.params.id,
+      }
+    });
+    res.status(200).json(deletedProduct);
+  } catch (err) {
+    res.status(400).json(err);
+    console.log(err);
+  }
 });
 
 module.exports = router;
